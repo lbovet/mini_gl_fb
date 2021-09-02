2eedfe56e439632916d3b716fa1737cecb5dad54
Laurent Bovet <laurent.bovet@windmaster.ch>
2021-09-02 11:43:57 +0200

    optimized buffer copy

--- examples/camera.rs
+++ examples/camera.rs
@@ -1,21 +1,26 @@
 #[macro_use]
 extern crate mini_gl_fb;
 
-use std::time::{Duration, Instant};
+use std::{
+    convert::TryInto,
+    time::{Duration, Instant},
+};
 
 use glutin::event::VirtualKeyCode;
-use mini_gl_fb::BufferFormat;
-use mini_gl_fb::glutin::event_loop::EventLoop;
 use mini_gl_fb::glutin::dpi::LogicalSize;
+use mini_gl_fb::glutin::event_loop::EventLoop;
+use mini_gl_fb::BufferFormat;
 
 use opencv::{
     core::{Mat, Vec3b},
     prelude::*,
-    Result,
-    videoio,
+    videoio, Result,
 };
 
 fn main() -> Result<()> {
+    let width: usize = 640;
+    let height: usize = 480;
+    let size: usize = width * height * 3;
 
     #[cfg(ocvrs_opencv_branch_32)]
     let mut cam = videoio::VideoCapture::new_default(0)?; // 0 is the default camera
@@ -23,20 +28,23 @@ fn main() -> Result<()> {
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
     let opened = videoio::VideoCapture::is_opened(&cam)?;
     if !opened {
-            panic!("Unable to open default camera!");
+        panic!("Unable to open default camera!");
     }
 
     let mut event_loop = EventLoop::new();
-    let mut fb = mini_gl_fb::get_fancy(config! {
-        window_title: String::from("Hello world!"),
-        window_size: LogicalSize::new(800.0, 600.0),
-        buffer_size: Some(LogicalSize::new(640, 480)),
-        invert_y: false,
-        resizable: false
-    }, &event_loop);
+    let mut fb = mini_gl_fb::get_fancy(
+        config! {
+            window_title: String::from("Hello world!"),
+            window_size: LogicalSize::new(800.0, 600.0),
+            buffer_size: Some(LogicalSize::new(width.try_into().unwrap(), height.try_into().unwrap())),
+            invert_y: false,
+            resizable: false
+        },
+        &event_loop,
+    );
 
-    fb.change_buffer_format::<u8>(BufferFormat::RGBA);
-    let mut buffer = vec![0; 640*480*4];
+    fb.change_buffer_format::<u8>(BufferFormat::BGR);
+    let mut buffer: Vec<u8> = vec![128; size];
 
     let mut update_id: Option<u32> = None;
 
@@ -44,29 +52,24 @@ fn main() -> Result<()> {
         input.wait = true;
 
         if update_id.is_none() {
-            update_id = Some(input.schedule_wakeup(Instant::now() + Duration::from_millis(5)))
+            update_id = Some(input.schedule_wakeup(Instant::now() + Duration::from_millis(10)))
         } else if let Some(mut wakeup) = input.wakeup {
             if Some(wakeup.id) == update_id {
                 let mut frame = Mat::default();
-                cam.read(&mut frame);
-                let frame_data_vec = Mat::data_typed::<Vec3b>(&frame);
-                let mut offset = 0;
-
-                if let Ok(data) = frame_data_vec {
-                    for pixel in data {
-                        let pixel_slice = &**pixel;
-                            buffer[offset] = pixel_slice[2];
-                            buffer[offset+1] = pixel_slice[1];
-                            buffer[offset+2] = pixel_slice[0];
-                            buffer[offset+3] = 128;
-                            offset += 4;
+                if let Ok(true) = cam.read(&mut frame) {
+                    let b = &mut *buffer;
+                    unsafe {
+                        match Mat::data_typed_unchecked::<u8>(&frame.reshape(1, 1).unwrap()) {
+                            Ok(data) => {
+                                b[0..size].copy_from_slice(data)
+                            }
+                            Err(why) => panic!("{}", why),
+                        }
                     }
+                    fb.update_buffer(&buffer);
                 }
 
-                fb.update_buffer(&buffer);
-
                 wakeup.when = Instant::now() + Duration::from_millis(5);
-
                 input.reschedule_wakeup(wakeup);
             }
 
@@ -75,7 +78,7 @@ fn main() -> Result<()> {
         }
 
         if input.key_is_down(VirtualKeyCode::Escape) {
-            return false;
+            panic!("Bye")
         }
 
         true
